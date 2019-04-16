51e79b7cfd656a9e30b45f3f76581f2008f68a59
elinahovakimyan <elinahovakimyan@gmail.com>
2019-04-16 13:36:48 +0400

    Updates

---
+++ precache-manifest.4adcda70ba90641d6553713d7494376c.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "8bab732c217955f87f5a9ae437ce9469",
+    "url": "/eventor-frontend/static/media/fork.8bab732c.svg"
+  },
+  {
+    "revision": "c07aaffcfc16b630586a",
+    "url": "/eventor-frontend/static/css/main.d7140e07.chunk.css"
+  },
+  {
+    "revision": "78a1a1c0b14a00dc026c",
+    "url": "/eventor-frontend/static/js/runtime~main.9f463351.js"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/eventor-frontend/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "e672e8d9c4137e8f9da1",
+    "url": "/eventor-frontend/static/js/2.69fb8a70.chunk.js"
+  },
+  {
+    "revision": "4d5f135ba15ffdfaef7df5118440016c",
+    "url": "/eventor-frontend/static/media/birthday-girl.4d5f135b.jpg"
+  },
+  {
+    "revision": "ff41e9c55258eb79e89951d32af16eef",
+    "url": "/eventor-frontend/static/media/balloons.ff41e9c5.svg"
+  },
+  {
+    "revision": "6dc749cfa06bdb4d75e699839e70a9d0",
+    "url": "/eventor-frontend/static/media/cake.6dc749cf.svg"
+  },
+  {
+    "revision": "6975706187e43713128cfd7982b584e6",
+    "url": "/eventor-frontend/static/media/confetti.69757061.svg"
+  },
+  {
+    "revision": "756a1d8748637868ab97e47e7781988e",
+    "url": "/eventor-frontend/static/media/cutlery.756a1d87.svg"
+  },
+  {
+    "revision": "d5524712f038987d5839c94f66b91667",
+    "url": "/eventor-frontend/static/media/donatello.d5524712.svg"
+  },
+  {
+    "revision": "da7d35a951bfcfab5233c56fe4163263",
+    "url": "/eventor-frontend/static/media/flags.da7d35a9.svg"
+  },
+  {
+    "revision": "c07aaffcfc16b630586a",
+    "url": "/eventor-frontend/static/js/main.24961529.chunk.js"
+  },
+  {
+    "revision": "012836a016294d98f65d2c722fa5f550",
+    "url": "/eventor-frontend/static/media/laugh.012836a0.svg"
+  },
+  {
+    "revision": "e181326f820871a8f2e769dc6a81974c",
+    "url": "/eventor-frontend/static/media/photo-camera.e181326f.svg"
+  },
+  {
+    "revision": "6e6b138b9b1efd72e1f510e51e33b485",
+    "url": "/eventor-frontend/static/media/present-colorful.6e6b138b.svg"
+  },
+  {
+    "revision": "7ee171e576a297f45e8af9109b5ed472",
+    "url": "/eventor-frontend/static/media/present-delivery.7ee171e5.svg"
+  },
+  {
+    "revision": "bf889f7073aeccabcb56ec62c88183c2",
+    "url": "/eventor-frontend/static/media/present.bf889f70.svg"
+  },
+  {
+    "revision": "19a2b200f8ac6186aa63a683f0520dc4",
+    "url": "/eventor-frontend/static/media/cake-animated.19a2b200.gif"
+  },
+  {
+    "revision": "1934d86c0fffef25d28964517249afe8",
+    "url": "/eventor-frontend/static/media/logo.1934d86c.png"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/eventor-frontend/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/eventor-frontend/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/eventor-frontend/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "e672e8d9c4137e8f9da1",
+    "url": "/eventor-frontend/static/css/2.9804f024.chunk.css"
+  },
+  {
+    "revision": "908600ca790f46516eca913a95a15773",
+    "url": "/eventor-frontend/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.152a1437db48a9d64dd4183ca510587d.js"
+  "/eventor-frontend/precache-manifest.4adcda70ba90641d6553713d7494376c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/eventor-frontend/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
