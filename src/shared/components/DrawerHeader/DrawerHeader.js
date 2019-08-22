3fe2c1c79f2256f332ef75e22d82a9c2b237cc2d
elinahovakimyan <elinahovakimyan@gmail.com>
2019-08-22 16:13:58 +0400

    Update website colors and header

--- src/shared/components/DrawerHeader/DrawerHeader.js
+++ src/shared/components/DrawerHeader/DrawerHeader.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Drawer, Icon } from 'antd';
+import React from 'react';
+import { Icon } from 'antd';
 // import { Link } from 'react-router-dom';
 
 import { Cart } from 'shared/components';
@@ -8,60 +8,29 @@ import './DrawerHeader.scss';
 
 function DrawerHeader() {
   const logoSrc = require('assets/logo.png');
-  const [visible, updateVisibility] = useState(false);
+  // const [visible, updateVisibility] = useState(false);
 
   return (
     <div id="header">
       <div className="header-content">
 
-        <a href="https://eventor.am">
-          <img className="logo-img" src={logoSrc} alt="logo" />
-        </a>
-        <h4>
-          <Icon type="phone" />
-          +374 95 661815, +374 93 661815
-        </h4>
-        <div>
+        <div className="inner-content">
+          <a href="https://eventor.am">
+            <img className="logo-img" src={logoSrc} alt="logo" />
+          </a>
+          <h4 className="desktop-contacts">
+            <Icon type="phone" />
+            +37495661815, +37493661815
+          </h4>
           <Cart />
-
-          <Icon
-            type="menu"
-            className="header-icon"
-            onClick={() => updateVisibility(true)}
-          />
-          <Drawer
-            closable
-            title="Ցանկ"
-            placement="right"
-            onClose={() => updateVisibility(false)}
-            visible={visible}
-            className="drawer-menu"
-          >
-            {/* <Link><Link to="/faq">Հաճախ Տրվող Հարցեր</Link></Link>
-              <p><Link to="/faq">Կապ Մեզ Հետ</Link></p>
-              <p><Link to="/faq">Բաժանորդագրվել</Link></p> */}
-
-            <h3>
-              Մանրամասների համար՝
-            </h3>
-            <h4>
-              <Icon type="phone" />
-              +37477067103
-            </h4>
-            <h4>
-              <Icon type="phone" />
-              +37495661815
-            </h4>
-
-            <a href="https://fb.com/eventor.am" target="_blank" rel="noopener noreferrer">
-              <Icon type="facebook" className="header-icon" />
-            </a>
-            <a href="https://instagram.com/eventor.am" target="_blank" rel="noopener noreferrer">
-              <Icon type="instagram" className="header-icon" />
-            </a>
-          </Drawer>
         </div>
+
+        <h4 className="mobile-contacts">
+          <Icon type="phone" />
+          +37495661815, +37493661815
+        </h4>
       </div>
+
     </div>
   );
 }
