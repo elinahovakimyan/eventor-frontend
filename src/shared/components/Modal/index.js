1da018af8323b53107433e37207c757a4f4b24de
elinahovakimyan <elinahovakimyan@gmail.com>
2019-09-08 09:00:13 +0400

    Fix modal close functionality

--- src/shared/components/Modal/index.js
+++ src/shared/components/Modal/index.js
@@ -4,13 +4,20 @@ import React from 'react';
 import ReactModal from 'react-modal';
 import { Icon } from 'antd';
 
+ReactModal.setAppElement('#root');
 
 function Modal({
   isOpen, onClose, className, children,
 }) {
 
   return (
-    <ReactModal shouldCloseOnEsc shouldCloseOnOverlayClick isOpen={isOpen} className={`Modal ${className}`}>
+    <ReactModal
+      shouldCloseOnEsc
+      shouldCloseOnOverlayClick
+      isOpen={isOpen}
+      onRequestClose={onClose}
+      className={`Modal ${className}`}
+    >
       <div className="Modal__box">
         <Icon type="close" className="Modal__box__close" onClick={onClose} />
 
