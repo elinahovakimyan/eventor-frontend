5376de11094e9b0e6ad35f53637329f02481f058
elinahovakimyan <elinahovakimyan@gmail.com>
2019-09-21 16:20:50 +0400

    Change campaign backers number

--- src/pages/Campaign/Campaign.js
+++ src/pages/Campaign/Campaign.js
@@ -6,7 +6,7 @@ import './Campaign.scss';
 
 function Campaign() {
   const [isFormOpen, toggleOpen] = useState(false);
-  const [backers, updateBackers] = useState(9);
+  const [backers, updateBackers] = useState(21);
   const [values, updateValues] = useState({});
   const [btnText, changeBtnText] = useState('Submit');
 
